4944ba4c4d3d6d83a197079ab5eb4ec07cab74db
Jason Quaccia <jlquaccia@gmail.com>
2017-04-23 17:53:55 -0700

    configured scss to work with the site

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // modules =================================================
 require('dotenv').config();
 var express = require('express');
+var sassMiddleware = require('node-sass-middleware');
+var path = require('path');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var mongoose = require('mongoose');
@@ -17,14 +19,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(express.static(__dirname + '/public'));
+app.use(sassMiddleware({
+  src: __dirname + '/sass',
+  dest: path.join(__dirname, '/public/css'),
+  // debug: true,
+  outputStyle: 'compressed',
+  prefix: '/css'
+}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // routes ==================================================
 require('./app/routes')(app);
 
 // start app ===============================================
 app.listen(port);
-console.log('Cedar Park is playing at my house!');
+console.log('cedar park is playing at my house!');
 
 // expose app ===============================================
 exports = module.exports = app;
